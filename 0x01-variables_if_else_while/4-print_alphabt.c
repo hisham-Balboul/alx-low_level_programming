8847dc9824c1e89390fb5568e92a92e06d069a36
Hisham Balboul <hishamsabry-97@hotmail.com>
2023-06-16 13:36:31 -0700

    4 script

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+	int i = 0;
+
+	while (i < 26)
+	{
+		if (c == 'q' || c == 'e')
+		{
+			i++;
+		}
+		putchar(c + i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
