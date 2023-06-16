72f0798356824d73afda3581f0dad9f0fa88ae39
Hisham Balboul <hishamsabry-97@hotmail.com>
2023-06-16 13:30:37 -0700

    3 script

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+	int i = 0;
+
+	while (i < 26)
+	{
+		putchar(c + i);
+		i++;
+	}
+	c = 'A';
+	i = 0;
+	while (i < 26)
+	{
+		putchar(c + i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
