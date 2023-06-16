526a0b39b23615515d04e900f28de84fa65962b1
Hisham Balboul <hishamsabry-97@hotmail.com>
2023-06-16 13:52:10 -0700

    6 script

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	short i = 49;
+	int j = 0;
+
+	while (j < 10)
+	{
+		putchar(i);
+		i++;
+		j++;
+	}
+	putchar('\n');
+	return (0);
