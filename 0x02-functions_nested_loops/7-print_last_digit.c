fae496f8cdbfb60ab6e54e24e6069cd7eac56e3f
Hisham Balboul <hishamsabry-97@hotmail.com>
2023-06-20 09:33:03 -0700

    7 script

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit- prints the last digit
+ * @n: is the number to be checked
+ * Return: the abs of a number.
+ */
+int print_last_digit(int n)
+{
+	int x = 0;
+	
+	x = n & % 10;
+	_putchar(x);
+	return (x);
+}
