c2e8066b08c74cfee62b862bd92e4ab5ba0e42c6
Hisham Balboul <hishamsabry-97@hotmail.com>
2023-06-26 06:56:33 -0700

    4 C file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+        int i;
+        
+        for (i = 0; *s != '\0'; i++)
+	{
+		s++;
+	}
+	s = s + i;
+	while (i != 0)
+	{
+		_putchar(*s);
+		s--;
+		i--;
+	}
+	_putchar('\n');
+}
