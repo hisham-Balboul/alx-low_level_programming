95fb571095f5259ce0057058fe70a5d054676f3d
Hisham Balboul <hishamsabry-97@hotmail.com>
2023-08-07 09:30:05 -0700

    1 C Filee

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,8 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len]; len++)
 	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd == -1 || w == -1)
+	wr = write(fd, text_content, len);
+	if (fd == -1 || wr == -1)
 		return (-1);
 
 	close(fd);
