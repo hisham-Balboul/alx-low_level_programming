efa5b1c529ebfd85941fd72ff3418176dc8a866d
Hisham Balboul <hishamsabry-97@hotmail.com>
2023-09-14 04:29:38 -0700

    0 C Filee

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,10 +13,7 @@ size_t print_dlistint(const dlistint_t *h)
 
 	if (h == NULL)
 		return (count);
-	/**while (h->prev != NULL)
-		  h = h->prev;
-	**/
-	  while (h != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		count++;
